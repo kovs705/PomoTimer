4698eedb427c6110b158f8e1c18538e30f4d140c
kovs705 <smartkovsik@gmail.com>
2022-04-20 16:49:55 +0300

    added new files for tableView cell and UITableViewViewController. Changes in storyboard with cells

--- PomoTimer/VCs/ViewController.swift
+++ PomoTimer/VCs/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
         // scrollView.isScrollEnabled = true
         scrollView.bounces = true
         scrollView.alwaysBounceVertical = true
